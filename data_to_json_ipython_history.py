768e341859cd37dcc6ada98e4f441f98d5180972
Paul Bacchus <paulsbacchus@gmail.com>
2013-02-04 17:47:46 -0800

    Updated the Collegues.json file to fix an error.

--- data_to_json_ipython_history.py
+++ data_to_json_ipython_history.py
@@ -93,7 +93,7 @@ skill2 = [i[1:] for i in skill2]
 will2 = [i[1:] for i in will2]
 
 colleagues_d = {
-    'AGREE': {'headers': agree[1], 'values': agree[1:13], 'footer': [13]},
+    'AGREE': {'headers': agree[1], 'values': agree[2:13], 'footer': [13]},
     'DISAGREE': {'headers': agree[16], 'values': agree[17:], 'footer': [28]},
     'Skill': skill2,
     'Will': will2
