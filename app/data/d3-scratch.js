fe7c42ce11436842861c5945d37e71e6dc383f63
Mark Reveley <mreveley@gmail.com>
2013-02-05 17:51:38 -0800

    cleaned up, moved combo dual highchart to own file

--- app/data/d3-scratch.js
+++ app/data/d3-scratch.js
@@ -4,7 +4,7 @@
 
 
 <style>
- 
+
 body {
   font: 10px sans-serif;
 }
@@ -15,61 +15,61 @@ body {
   stroke: #000;
   shape-rendering: crispEdges;
 }
- 
+
 .bar {
   fill: #7EC0EE;
   stroke: black;
   stroke-width: .8;
 }
- 
+
 .x.axis path {
   display: none;
 }
- 
+
 </style>
 
 <script>
 
-var margin = {top: 20, right: 20, bottom: 30, left: 40},  
-    width = 600 - margin.left - margin.right,             
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 600 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
 
 /* this establishes our x axis scale as being ordinal, or a discreet mapping of the domain to the range */
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
- 
+
 var y = d3.scale.linear()
     .range([height, 0]);
- 
+
 var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
- 
+
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left")
     // .tickFormat(formatYNumbers);
- 
+
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
- 
+
 d3.csv("data/monthly-perf-actual.csv", function(error, data) {
- 
+
   data.forEach(function(d) {
     d.amount = +d.amount;
   });
- 
+
   x.domain(data.map(function(d) { return d.date; }));
   y.domain([0, d3.max(data, function(d) { return d.amount; })]);
- 
+
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
- 
+
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
@@ -79,7 +79,7 @@ d3.csv("data/monthly-perf-actual.csv", function(error, data) {
       .attr("dy", ".71em")
       .style("text-anchor", "end")
       .text("");
- 
+
   svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
@@ -90,24 +90,181 @@ d3.csv("data/monthly-perf-actual.csv", function(error, data) {
       .attr("height", function(d) { return height - y(d.amount); })
       .attr("rx", 11)
       .attr("ry", 11);
- 
- 
+
+
 });
- 
+
 </script>
 
 
 op cost ytd
 
-BU Breakdown YTD,R&WO,WS,CrOp,S&OP,C/S,GP,Fraud,Other,YTD Total
-CMU 1,-5177204,6053246,7324421,848662,6185788,175483,4437194,-5901323,-7196342.0
+
+// var text = svg.selectAll(grect)
+// .append("text")
+//   .text(function (d) {
+//     return "test"});
+
+// .attr("x", function(d) { return d.row * w; })
+// .attr("y", function(d) { return d.col * h; })
 
 
-CMU 2,-3078878,"£79,813","£1,200,202","-£1,544,313","-£5,291,301","-£1,495,406","£2,853,590","£7,751,900","£17,074,488.0"
-CMU 3,-6663317,"£466,469","£5,380,679","-£2,538,474","-£1,864,961","£2,185,401","-£3,424,598","£7,668,049","£5,358,390.0"
-CMU 4,3052221,"-£2,402,248","£3,077,265","-£138,791","-£2,997,250","-£6,876,752","£4,536,282","-£4,815,914","-£19,520,929.0"
-CMU 5,-4568966,"£672,232","£2,013,017","£5,950,261","£3,432,369","-£218,781","£7,663,167","-£5,891,058","-£6,464,361.0"
-CMU 6,-6710873,"£7,865,603","-£4,741,101","£21,837","£3,510,269","-£6,686,917","£5,108,147","-£7,619,006","£14,020,104.0"
-CMU 7,-7700024,"£742,627","-£7,498,896","£1,703,092","-£4,833,602","-£844,290","£4,124,656","£4,209,420","-£3,946,551.0"
-CMU 8,-4151496,"-£1,413,538","£818,267","£5,847,434","-£1,679,860","-£5,988,175","£7,964,900","-£649,104","£7,452,443.0"
-Total,-34998537,"£12,064,204","£7,573,854","£10,149,708","-£3,538,548","-£19,749,437","£33,263,338","-£5,247,036","£21,814,161"
+
+  // .append("text")
+  // .text(function (d) {
+  //   return "test"});
+  // .data(data).enter().append("g");
+
+// var heatMap = svg.selectAll(".heatmap")
+//   .data(data, function(d) { return d.col + ':' + d.row; })
+// .enter().append("svg:rect")
+//   .attr("x", function(d) { return d.row * w; })
+//   .attr("y", function(d) { return d.col * h; })
+//   .attr("width", function(d) { return w / 1.1; })
+//   .attr("height", function(d) { return h / 1.15; })
+//   .style("fill", function(d) { return colorScale(d.score); })
+//   .append("text")
+//   .text(function (d) {
+//     return "test"});
+
+// ----------
+
+// d3.csv("operations-cost-2-ytd.csv", function (csv) {
+// csv.forEach(function (o) {
+// o.row = parseInt(o.row) - 1;
+// o.col = parseInt(o.col) - 1;
+// o.value = parseFloat(o.value);
+// });
+
+// var grect = svg.selectAll("rect.combo")
+// .data(csv).enter().append("g");
+
+// var rect = grect.append("rect")
+// .attr("class", "combo")
+// .attr("width", cellSize)
+// .attr("height", cellSize)
+// .attr("x",function (d) {
+//   return d.col * cellSize;
+//   })
+// .attr("y", function (d) {
+//   return d.row * cellSize;
+//   })
+// .attr("fill", function (d) {
+//   return color(d.value);
+//   });
+
+// var txt = grect.append("text").text(function (d) {
+// return roundNumber(d.value, 1);
+// }).attr("x",function (d) {
+// return( d.col * cellSize + cellSize / 2);
+// }).attr("y",function (d) {
+// return( d.row * cellSize + cellSize / 2);
+// }).attr('text-anchor', 'middle')
+// .style("fill", "white").style("stroke-width", 1.5);
+
+// });
+
+
+
+
+
+// d3.csv("operations-cost-2-ytd.csv", function(data) {
+//     data.forEach(function(d) {
+//     d.score = +d.score;
+//     d.row = +d.row;
+// d.col = +d.col;
+// });
+
+//height of each row in the heatmap
+//width of each column in the heatmap
+
+
+
+// ----------
+
+// <script>
+// d3.csv("sgadata.csv", function(data) {
+//     data.forEach(function(d) {
+//     d.score = +d.score;
+//     d.row = +d.row;
+// d.col = +d.col;
+// });
+// //height of each row in the heatmap
+// //width of each column in the heatmap
+// var h = gridSize;
+// var w = gridSize;
+// var rectPadding = 60;
+
+// $('#heatmap-canvas').empty();
+
+// var mySVG = d3.select("#heatmap-canvas")
+// .style('top',0)
+// .style('left',0);
+
+// var colorScale = d3.scale.linear()
+// .domain([-1, 0, 1])
+// .range([colorLow, colorMed, colorHigh]);
+
+// rowNest = d3.nest()
+// .key(function(d) { return d.row; })
+// .key(function(d) { return d.col; });
+
+// dataByRows = rowNest.entries(data);
+// mySVG.forEach(function(){
+// var heatmapRow = mySVG.selectAll(".heatmap")
+//     .data(dataByRows, function(d) { return d.key; })
+//     .enter().append("g");
+
+//     //For each row, generate rects based on columns - this is where I get stuck
+//     heatmapRow.forEach(function(){
+//     var heatmapRects = heatmapRow
+//         .selectAll(".rect")
+//         .data(function(d) {return d.score;})
+//         .enter().append("svg:rect")
+//                     .attr('width',w)
+//         .attr('height',h)
+//         .attr('x', function(d) {return (d.row * w) + rectPadding;})
+//         .attr('y', function(d) {return (d.col * h) + rectPadding;})
+//         .style('fill',function(d) {
+//             if(d.score == NaN){return colorNA;}
+//             return colorScale(d.score);
+//                  })
+
+// })
+// </script>
+
+
+// ---- works
+
+// var gridSize = 70,
+//   h = 38,
+//   w = 67,
+//   rectPadding = 60;
+
+// var colorLow = 'green', colorMed = 'yellow', colorHigh = 'red';
+
+// var margin = {top: 20, right: 80, bottom: 30, left: 50},
+//   width = 640 - margin.left - margin.right,
+//   height = 500 - margin.top - margin.bottom;
+
+// var colorScale = d3.scale.linear()
+//    .domain([-1, 0, 1])
+//    .range([colorLow, colorMed, colorHigh]);
+
+// var svg = d3.select("#heatmap").append("svg")
+//   .attr("width", width + margin.left + margin.right)
+//   .attr("height", height + margin.top + margin.bottom)
+// .append("g")
+//   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// var heatMap = svg.selectAll(".heatmap")
+//   .data(data, function(d) { return d.col + ':' + d.row; })
+// .enter().append("svg:rect")
+//   .attr("x", function(d) { return d.row * w; })
+//   .attr("y", function(d) { return d.col * h; })
+//   .attr("width", function(d) { return w / 1.1; })
+//   .attr("height", function(d) { return h / 1.15; })
+//   .style("fill", function(d) { return colorScale(d.score); })
+//   .append("text")
+//   .text(function (d) {
+//     return "test"});
--- app/js/operations-cost-heatmap.js
+++ app/js/operations-cost-heatmap.js
@@ -15,15 +15,6 @@ var data2 = [
 {num: 23, num: 45}
 ]
 
-// d3.csv("operations-cost-2-ytd.csv", function(data) {
-//     data.forEach(function(d) { 
-//     d.score = +d.score;
-//     d.row = +d.row;
-// d.col = +d.col;
-// });
-
-//height of each row in the heatmap
-//width of each column in the heatmap
 var gridSize = 70,
   h = 43,
   w = 67,
@@ -70,167 +61,19 @@ textType.text("hello")
 textType.attr("x", function(d) { return d.row * w; })
 .attr("y", function(d) { return d.col * h; })
 
-// var text = svg.selectAll(grect)
-// .append("text")
-//   .text(function (d) { 
-//     return "test"});
-
-// .attr("x", function(d) { return d.row * w; })
-// .attr("y", function(d) { return d.col * h; })
-  
-
-
-  // .append("text")
-  // .text(function (d) { 
-  //   return "test"});
-  // .data(data).enter().append("g");
-
-// var heatMap = svg.selectAll(".heatmap")
-//   .data(data, function(d) { return d.col + ':' + d.row; })
-// .enter().append("svg:rect")
-//   .attr("x", function(d) { return d.row * w; })
-//   .attr("y", function(d) { return d.col * h; })
-//   .attr("width", function(d) { return w / 1.1; })
-//   .attr("height", function(d) { return h / 1.15; })
-//   .style("fill", function(d) { return colorScale(d.score); })
-//   .append("text")
-//   .text(function (d) { 
-//     return "test"});
-
-// ----------
-
-// d3.csv("operations-cost-2-ytd.csv", function (csv) {
-// csv.forEach(function (o) {
-// o.row = parseInt(o.row) - 1;
-// o.col = parseInt(o.col) - 1;
-// o.value = parseFloat(o.value);
-// });
-
-// var grect = svg.selectAll("rect.combo")
-// .data(csv).enter().append("g");
-
-// var rect = grect.append("rect")
-// .attr("class", "combo")
-// .attr("width", cellSize)
-// .attr("height", cellSize)
-// .attr("x",function (d) {
-//   return d.col * cellSize;
-//   })
-// .attr("y", function (d) {
-//   return d.row * cellSize;
-//   })
-// .attr("fill", function (d) {
-//   return color(d.value);
-//   });
-
-// var txt = grect.append("text").text(function (d) {
-// return roundNumber(d.value, 1);
-// }).attr("x",function (d) {
-// return( d.col * cellSize + cellSize / 2);
-// }).attr("y",function (d) {
-// return( d.row * cellSize + cellSize / 2);
-// }).attr('text-anchor', 'middle')
-// .style("fill", "white").style("stroke-width", 1.5);
-
-// });
-
-
-
+// BU Breakdown YTD,R&WO,WS,CrOp,S&OP,C/S,GP,Fraud,Other,YTD Total
+// CMU 1,-5177204,6053246,7324421,848662,6185788,175483,4437194,-5901323,-7196342.0
+// CMU 2,-3078878,"£79,813","£1,200,202","-£1,544,313","-£5,291,301","-£1,495,406","£2,853,590","£7,751,900","£17,074,488.0"
+// CMU 3,-6663317,"£466,469","£5,380,679","-£2,538,474","-£1,864,961","£2,185,401","-£3,424,598","£7,668,049","£5,358,390.0"
+// CMU 4,3052221,"-£2,402,248","£3,077,265","-£138,791","-£2,997,250","-£6,876,752","£4,536,282","-£4,815,914","-£19,520,929.0"
+// CMU 5,-4568966,"£672,232","£2,013,017","£5,950,261","£3,432,369","-£218,781","£7,663,167","-£5,891,058","-£6,464,361.0"
+// CMU 6,-6710873,"£7,865,603","-£4,741,101","£21,837","£3,510,269","-£6,686,917","£5,108,147","-£7,619,006","£14,020,104.0"
+// CMU 7,-7700024,"£742,627","-£7,498,896","£1,703,092","-£4,833,602","-£844,290","£4,124,656","£4,209,420","-£3,946,551.0"
+// CMU 8,-4151496,"-£1,413,538","£818,267","£5,847,434","-£1,679,860","-£5,988,175","£7,964,900","-£649,104","£7,452,443.0"
+// Total,-34998537,"£12,064,204","£7,573,854","£10,149,708","-£3,538,548","-£19,749,437","£33,263,338","-£5,247,036","£21,814,161"
 
 }
 
 window.onload = load;
 
 
-
-
-
-
-// ----------
-
-// <script>
-// d3.csv("sgadata.csv", function(data) {
-//     data.forEach(function(d) { 
-//     d.score = +d.score;
-//     d.row = +d.row;
-// d.col = +d.col;
-// });
-// //height of each row in the heatmap
-// //width of each column in the heatmap
-// var h = gridSize;
-// var w = gridSize;
-// var rectPadding = 60;
-
-// $('#heatmap-canvas').empty();
-
-// var mySVG = d3.select("#heatmap-canvas")
-// .style('top',0)
-// .style('left',0);
-
-// var colorScale = d3.scale.linear()
-// .domain([-1, 0, 1])
-// .range([colorLow, colorMed, colorHigh]);
-
-// rowNest = d3.nest()
-// .key(function(d) { return d.row; })
-// .key(function(d) { return d.col; });
-
-// dataByRows = rowNest.entries(data);
-// mySVG.forEach(function(){
-// var heatmapRow = mySVG.selectAll(".heatmap")
-//     .data(dataByRows, function(d) { return d.key; })
-//     .enter().append("g");
-
-//     //For each row, generate rects based on columns - this is where I get stuck
-//     heatmapRow.forEach(function(){
-//     var heatmapRects = heatmapRow
-//         .selectAll(".rect")
-//         .data(function(d) {return d.score;})
-//         .enter().append("svg:rect")
-//                     .attr('width',w)
-//         .attr('height',h)
-//         .attr('x', function(d) {return (d.row * w) + rectPadding;})
-//         .attr('y', function(d) {return (d.col * h) + rectPadding;})
-//         .style('fill',function(d) {
-//             if(d.score == NaN){return colorNA;}
-//             return colorScale(d.score);
-//                  })
-
-// })
-// </script>
-
-
-// ---- works
-
-// var gridSize = 70,
-//   h = 38,
-//   w = 67,
-//   rectPadding = 60;
-
-// var colorLow = 'green', colorMed = 'yellow', colorHigh = 'red';
-
-// var margin = {top: 20, right: 80, bottom: 30, left: 50},
-//   width = 640 - margin.left - margin.right,
-//   height = 500 - margin.top - margin.bottom;
-
-// var colorScale = d3.scale.linear()
-//    .domain([-1, 0, 1])
-//    .range([colorLow, colorMed, colorHigh]);
-
-// var svg = d3.select("#heatmap").append("svg")
-//   .attr("width", width + margin.left + margin.right)
-//   .attr("height", height + margin.top + margin.bottom)
-// .append("g")
-//   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// var heatMap = svg.selectAll(".heatmap")
-//   .data(data, function(d) { return d.col + ':' + d.row; })
-// .enter().append("svg:rect")
-//   .attr("x", function(d) { return d.row * w; })
-//   .attr("y", function(d) { return d.col * h; })
-//   .attr("width", function(d) { return w / 1.1; })
-//   .attr("height", function(d) { return h / 1.15; })
-//   .style("fill", function(d) { return colorScale(d.score); })
-//   .append("text")
-//   .text(function (d) { 
-//     return "test"});
