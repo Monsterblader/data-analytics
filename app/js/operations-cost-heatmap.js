840535fb4d23f2ba5a159ffdbd9933bd55b5300b
Mark Reveley <mreveley@gmail.com>
2013-02-05 17:19:54 -0800

    basic text overlay via d3 in heat chart

--- app/js/operations-cost-heatmap.js
+++ app/js/operations-cost-heatmap.js
@@ -10,6 +10,11 @@ function load() {
 {score: 1, row: 7, col: 0}, {score: 0, row: 7, col: 1}, {score: 0, row: 7, col: 2}, {score: 0, row: 7, col: 3}, {score: 0, row: 7, col: 4}, {score: 0, row: 7, col: 5}, {score: 0, row: 7, col: 6}, {score: 0, row: 7, col: 7}, {score: 0, row: 7, col: 8}, {score: 0, row: 7, col: 9},
 {score: 1, row: 8, col: 0}, {score: 0, row: 8, col: 1}, {score: 0, row: 8, col: 2}, {score: 0, row: 8, col: 3}, {score: 0, row: 8, col: 4}, {score: 0, row: 8, col: 5}, {score: 0, row: 8, col: 6}, {score: 0, row: 8, col: 7}, {score: 0, row: 8, col: 8}, {score: 0, row: 8, col: 9},
 ];
+
+var data2 = [
+{num: 23, num: 45}
+]
+
 // d3.csv("operations-cost-2-ytd.csv", function(data) {
 //     data.forEach(function(d) { 
 //     d.score = +d.score;
@@ -20,14 +25,14 @@ function load() {
 //height of each row in the heatmap
 //width of each column in the heatmap
 var gridSize = 70,
-  h = 38,
+  h = 43,
   w = 67,
   rectPadding = 60;
 
 var colorLow = 'green', colorMed = 'yellow', colorHigh = 'red';
 
 var margin = {top: 20, right: 80, bottom: 30, left: 50},
-  width = 640 - margin.left - margin.right,
+  width = 800 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
 var colorScale = d3.scale.linear()
@@ -40,14 +45,97 @@ var svg = d3.select("#heatmap").append("svg")
 .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var heatMap = svg.selectAll(".heatmap")
+
+var grect = svg.selectAll(".heatmap")
   .data(data, function(d) { return d.col + ':' + d.row; })
-.enter().append("svg:rect")
+.enter()
+.append("g")
   .attr("x", function(d) { return d.row * w; })
   .attr("y", function(d) { return d.col * h; })
-  .attr("width", function(d) { return w / 1.1; })
-  .attr("height", function(d) { return h / 1.15; })
-  .style("fill", function(d) { return colorScale(d.score); });
+  .attr("width", function(d) { return w / 1.05; })
+  .attr("height", function(d) { return h / 1.09; })
+.append("svg:rect")
+  .attr("x", function(d) { return d.row * w; })
+  .attr("y", function(d) { return d.col * h; })
+  .attr("width", function(d) { return w / 1.05; })
+  .attr("height", function(d) { return h / 1.09; })
+  .style("fill", function(d) { return colorScale(d.score); })
+
+var text = svg.selectAll("g")
+
+textType = text.append("text")
+
+textType.text("hello")
+
+textType.attr("x", function(d) { return d.row * w; })
+.attr("y", function(d) { return d.col * h; })
+
+// var text = svg.selectAll(grect)
+// .append("text")
+//   .text(function (d) { 
+//     return "test"});
+
+// .attr("x", function(d) { return d.row * w; })
+// .attr("y", function(d) { return d.col * h; })
+  
+
+
+  // .append("text")
+  // .text(function (d) { 
+  //   return "test"});
+  // .data(data).enter().append("g");
+
+// var heatMap = svg.selectAll(".heatmap")
+//   .data(data, function(d) { return d.col + ':' + d.row; })
+// .enter().append("svg:rect")
+//   .attr("x", function(d) { return d.row * w; })
+//   .attr("y", function(d) { return d.col * h; })
+//   .attr("width", function(d) { return w / 1.1; })
+//   .attr("height", function(d) { return h / 1.15; })
+//   .style("fill", function(d) { return colorScale(d.score); })
+//   .append("text")
+//   .text(function (d) { 
+//     return "test"});
+
+// ----------
+
+// d3.csv("operations-cost-2-ytd.csv", function (csv) {
+// csv.forEach(function (o) {
+// o.row = parseInt(o.row) - 1;
+// o.col = parseInt(o.col) - 1;
+// o.value = parseFloat(o.value);
+// });
+
+// var grect = svg.selectAll("rect.combo")
+// .data(csv).enter().append("g");
+
+// var rect = grect.append("rect")
+// .attr("class", "combo")
+// .attr("width", cellSize)
+// .attr("height", cellSize)
+// .attr("x",function (d) {
+//   return d.col * cellSize;
+//   })
+// .attr("y", function (d) {
+//   return d.row * cellSize;
+//   })
+// .attr("fill", function (d) {
+//   return color(d.value);
+//   });
+
+// var txt = grect.append("text").text(function (d) {
+// return roundNumber(d.value, 1);
+// }).attr("x",function (d) {
+// return( d.col * cellSize + cellSize / 2);
+// }).attr("y",function (d) {
+// return( d.row * cellSize + cellSize / 2);
+// }).attr('text-anchor', 'middle')
+// .style("fill", "white").style("stroke-width", 1.5);
+
+// });
+
+
+
 
 }
 
@@ -55,6 +143,10 @@ window.onload = load;
 
 
 
+
+
+
+// ----------
 
 // <script>
 // d3.csv("sgadata.csv", function(data) {
@@ -106,3 +198,39 @@ window.onload = load;
 
 // })
 // </script>
+
+
+// ---- works
+
+// var gridSize = 70,
+//   h = 38,
+//   w = 67,
+//   rectPadding = 60;
+
+// var colorLow = 'green', colorMed = 'yellow', colorHigh = 'red';
+
+// var margin = {top: 20, right: 80, bottom: 30, left: 50},
+//   width = 640 - margin.left - margin.right,
+//   height = 500 - margin.top - margin.bottom;
+
+// var colorScale = d3.scale.linear()
+//    .domain([-1, 0, 1])
+//    .range([colorLow, colorMed, colorHigh]);
+
+// var svg = d3.select("#heatmap").append("svg")
+//   .attr("width", width + margin.left + margin.right)
+//   .attr("height", height + margin.top + margin.bottom)
+// .append("g")
+//   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// var heatMap = svg.selectAll(".heatmap")
+//   .data(data, function(d) { return d.col + ':' + d.row; })
+// .enter().append("svg:rect")
+//   .attr("x", function(d) { return d.row * w; })
+//   .attr("y", function(d) { return d.col * h; })
+//   .attr("width", function(d) { return w / 1.1; })
+//   .attr("height", function(d) { return h / 1.15; })
+//   .style("fill", function(d) { return colorScale(d.score); })
+//   .append("text")
+//   .text(function (d) { 
+//     return "test"});
