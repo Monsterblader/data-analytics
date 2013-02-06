122b27d32e2c0f9801690e0204d1b6a852a20cd7
Paul Bacchus <paulsbacchus@gmail.com>
2013-02-05 18:37:07 -0800

    Updated the Capacity.json file with the correct labels. Add service for getting and formatting axis. Adding a chart in the controller.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,10 +15,36 @@ function HeadCountCtrl() {
 }
 // HeadCountCtrl.$inject = [];
 
-function CapacityCtrl($scope, CapacityData) {
-  $scope.capacityData = CapacityData.query();
+function CapacityCtrl($scope, $http, getXAxis, getTargetTotalColumnData) {
+  // var capacityData;
+  $http.get('../data/Capacity.json').success(function(data, status, headers, config){
+    // capacityData = data;
+    $scope.capacityData = data;
+    console.log($scope.capacityData);
+
+    // Extract axis, which is the same for 'Target' and 'Actual'
+    var targetXAxis = getXAxis($scope.capacityData);
+    // Extract data
+    var targetTotalColumnData = getTargetTotalColumnData($scope.capacityData);
+
+    // Get container elements
+    var $targetChartContainer = $('.target-volume');
+    var $actualChartContainer = $('.actual-volume');
+    // Create chart
+    var volumeChart = new Highcharts.Chart({
+      chart: {
+        renderTo: $targetChartContainer,
+        type: 'line'
+      },
+      xAxis: {
+        categories: targetXAxis
+      }
+    });
+  }).error(function(data, status, headers, config){
+    console.log(data, status, headers, config);
+  });
 }
-// CapacityCtrl.$inject = [];
+// CapacityCtrl.$inject = ['$scope', '$http'];
 
 function ColleaguesCtrl() {
 }
--- app/js/services.js
+++ app/js/services.js
@@ -1,9 +1,30 @@
 'use strict';
 
+var months = {
+   '1': 'Jan'
+  ,'2': 'Feb'
+  ,'3': 'Mar'
+  ,'4': 'Apr'
+  ,'5': 'May'
+  ,'6': 'Jun'
+  ,'7': 'Jul'
+  ,'8': 'Aug'
+  ,'9': 'Sep'
+  ,'10': 'Oct'
+  ,'11': 'Nov'
+  ,'12': 'Dec'
+};
+
 /* Services */
 
-angular.module('McApp.services', ['ngResource'])
-  .factory('CapacityData', function($resource){
-    return $resource('../data/Capacity.json', {}, {query: {method:'GET'}
+angular.module('McApp.services', [])
+  .factory('getXAxis', function(){
+    return function(data){
+      var values = data.Target.values;
+      var dates = _.map(values, function(row){
+        var info = row[0].slice(0, 3).reverse();
+        return ('0' + info[0]).slice(-2) + ' ' + months['' + info[1]] + ' ' + ('' + info[2]).slice(-2);
+      });
+      return dates;
+    };
   });
-});
