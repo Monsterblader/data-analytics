e254a2e5b13692e3e6f633b444948c321c205d21
Mark Reveley <mreveley@gmail.com>
2013-02-05 18:42:58 -0800

    added group operations  highcharts template, renamed the js files to reflect the charts

--- app/js/group-operations-chart.js
+++ app/js/group-operations-chart.js
@@ -33,7 +33,7 @@ $(function () {
                 }
             }, { // Secondary yAxis
                 title: {
-                    text: '', // Rainfall
+                    text: '', // actual
                     style: {
                         color: '#4572A7'
                     }
@@ -52,7 +52,7 @@ $(function () {
                 formatter: function() {
                     return ''+
                         this.x +': '+ this.y +
-                        (this.series.name == 'Rainfall' ? ' mm' : '°C');
+                        (this.series.name == 'actual' ? ' mm' : '°C');
                 }
             },
             legend: {
@@ -65,14 +65,14 @@ $(function () {
                 backgroundColor: '#FFFFFF'
             },
             series: [{
-                name: 'Rainfall',
+                name: 'Actual',
                 color: '#4572A7',
                 type: 'column',
                 yAxis: 1,
                 data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
 
             }, {
-                name: 'Temperature',
+                name: 'Target',
                 color: '#89A54E',
                 type: 'spline',
                 data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
