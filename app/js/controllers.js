7cb1432baa8eb107b38379804fef4e8ae838560d
Albert Lee <Monsterblader@gmail.com>
2013-02-08 14:48:26 -0800

    Dropdown menu working. Need to implement buttons, fix dropdown menu view and default value.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -17,8 +17,8 @@ function HeadCountCtrl($scope) {
 	$scope.headCountData = {percentage: {grOp: {left: "l1", right: "r1"}, BUBreak: {left: "l2", right: "r2"}},
 		 absolute: {grOp: {left: "l3", right: "r3"}, BUBreak: {left: "l4", right: "r4"}}};
   $scope.change = function (){
-    if ($scope.numTypeProp="typePerc") { $scope.imgLeft = "Moohaha"}
-      else {$scope.imgLeft = "Brouhaha"};
+    if ($scope.numTypeProp==="typePerc") { $scope.imgLeft = "Moohaha"; $scope.imgRight = "Brouhaha" }
+      else {$scope.imgLeft = "Brouhaha"; $scope.imgRight = "Moohaha" };
   }
 }
 // HeadCountCtrl.$inject = [];
