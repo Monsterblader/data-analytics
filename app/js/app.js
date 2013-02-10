5ba01515547bebea99222093ec76c9baa4b7a982
Sate <markc@Marks-MacBook-Air.local>
2013-02-09 20:57:10 -0800

    Cost tab and animations

--- app/js/app.js
+++ app/js/app.js
@@ -9,5 +9,12 @@ angular.module('McApp', ['McApp.capacityServices', 'McApp.filters'])
       .when('/headcount', {templateUrl: 'partials/headcount.html', controller: HeadCountCtrl})
       .when('/capacity', {templateUrl: 'partials/capacity.html', controller: CapacityCtrl})
       .when('/colleagues', {templateUrl: 'partials/colleagues.html', controller: ColleaguesCtrl})
+      .when('/cost', {templateUrl: 'partials/cost.html', controller: CostCtrl})
       .otherwise({redirectTo: '/home'});
 }]);
+
+function HomeFragmentController($scope) {
+    $scope.$on("$routeChangeSuccess", function (scope, next, current) {
+          $scope.transitionState = "blah"
+    });
+}
