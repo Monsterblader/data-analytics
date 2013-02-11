fbb83cb2ad54a5dd041dc90ace4eca4682d067a8
Paul Bacchus <paulsbacchus@gmail.com>
2013-02-11 11:40:15 -0800

    Removed dead link form side bar. Added url link to operational costs.

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,6 @@ angular.module('McApp', ['McApp.capacityServices', 'McApp.filters', 'headcountFi
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
       .when('/home', {templateUrl: 'partials/home.html', controller: HomeCtrl})
-      // .when('/operations_costs', {templateUrl: 'partials/operations_costs.html', controller: OperationsCtrl})
       .when('/headcount', {templateUrl: 'partials/headcount.html', controller: HeadCountCtrl})
       .when('/capacity', {templateUrl: 'partials/capacity.html', controller: CapacityCtrl})
       .when('/colleagues', {templateUrl: 'partials/colleagues.html', controller: ColleaguesCtrl})
